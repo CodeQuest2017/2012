330a38af40b9689de189161dda85cbcdb0a426e9
Donovan Glover <UltimateDonovan@gmail.com>
2017-03-22 18:52:35 -0400

    Add a failed attempt at #12 that I forgot to push

--- Donovan/Prob12.java
+++ Donovan/Prob12.java
@@ -3,93 +3,84 @@ import java.util.*;
 import java.io.*;
 
 public class Prob12 {
-    public static List<Boolean[]> maze = new ArrayList<Boolean[]>();
-    public static boolean[][] path; // Global to override in recursive call
+    public static Character[][] maze;
+    public static boolean[][] marked;
 	public static int M;
 	public static int N;
 	
 	public static void main(String[] args) throws IOException {
 	    init();
-		// For each block in col[0]:
-		for(int i = 0; i < M; i++) {
-		    // If the current block is a potential block A:
-		    if(maze.get(i)[0]) {
-		    	// While we are not at block B and we can move somewhere
-		    	boolean canMove = true;
-		    	boolean notB = true;
-		    	int currentRow = i;
-		    	int currentColumn = 0;
-		    	List<Integer[]> subpath = new ArrayList<Integer[]>(); 
-		    	// Store the path array
-		    	path = new boolean[M][N];
-		    	// Keep track of our last move
-		    	int lastMove = 0;
-		    	while(canMove && notB) {
-		    		boolean canMoveLeft = false;
-		    		boolean canMoveRight = false;
-		    		boolean canMoveUp = false;
-		    		boolean canMoveDown = false;
-		    		// If we can move somewhere else, move there
-		    		
-		    		// When we move somewhere, save that value in path[][]
-		    		// If we find a dead end, remove all values from path[][]
-		    		
-		    		// If we can move to more than one place, save that intersection
-		    		if(currentColumn > 0 && maze.get(currentRow)[currentColumn - 1] && lastMove != 2) canMoveLeft = true;
-		    		if(currentColumn + 1 < N && maze.get(currentRow)[currentColumn + 1] && lastMove != 1) canMoveRight = true;
-		    		if(currentRow + 1 < M && maze.get(currentRow + 1)[currentColumn] && lastMove != 4) canMoveUp = true;
-		    		if(currentRow > 0 && maze.get(currentRow - 1)[currentColumn] && lastMove != 3) canMoveDown = true;
-		    		
-		    		if(!canMoveLeft && !canMoveRight && !canMoveUp && !canMoveDown) {
-		    			System.out.println("Can't move anywhere at Row: " + currentRow + ", Column: " + currentColumn);
-		    			System.exit(0);
-		    		}
-		    		
-		    		// If we can only go one way, go that way
-		    		if(canMoveLeft ^ canMoveRight ^ canMoveUp ^ canMoveDown) {
-		    			if(canMoveLeft) {
-		    				path[currentRow][currentColumn - 1] = true;
-		    				currentColumn--;
-		    				lastMove = 1;
-		    			}
-		    			if(canMoveRight) {
-		    				path[currentRow][currentColumn + 1] = true;
-		    				currentColumn++;
-		    				lastMove = 2;
-		    			}
-		    			if(canMoveUp) {
-		    				path[currentRow - 1][currentColumn] = true;
-		    				currentRow--;
-		    				lastMove = 3;
-		    			}
-		    			if(canMoveDown) {
-		    				path[currentRow + 1][currentColumn] = true;
-		    				currentRow++;
-		    				lastMove = 4;
-		    			}
-		    		} else {
-		    			// Otherwise, we can go more than one way
-		    			System.out.println("We can go more than one way at: " + currentColumn + ", " + currentRow);
-		    			break;
-		    		}
-		    	}
-		    }
-		}
+	    for(int i = 0; i < M; i++) {
+	    	if(maze[i][0] == 'O') {
+	    		System.out.println(solveMaze(i, 0));
+	    	}
+	    }
 	}
-	
+	public static Stack R = new Stack();
+	public static Stack C = new Stack();
+	public static int solveMaze(int row, int col) {
+		System.out.println("row: " + row + ", col: " + col);
+		// If we reached the end of the maze
+		if(col == N - 1) {
+			return 1;
+		} else {
+			boolean down = row + 1 < M && maze[row + 1][col] == 'O' && !marked[row + 1][col];
+			boolean left = col > 0 && maze[row][col - 1] == 'O' && !marked[row][col - 1];
+			boolean up = row > 0 && maze[row - 1][col] == 'O' && !marked[row - 1][col];
+			boolean right = col + 1 < N && maze[row][col + 1] == 'O' && !marked[row][col + 1];
+			int numMoves = 0;
+			numMoves += down ? 1 : 0;
+			numMoves += left ? 1 : 0;
+			numMoves += up ? 1 : 0;
+			numMoves += down ? 1 : 0;
+			if(numMoves >= 2) {
+				R.push(row);
+				C.push(col);
+			}
+			// Can we go down?
+			if(down) {
+				marked[row][col] = true;
+				return solveMaze(row + 1, col);
+			}
+			// Left?
+			if(left) {
+				marked[row][col] = true;
+				return solveMaze(row, col - 1);
+			}
+			// Up?
+			if(up) {
+				marked[row][col] = true;
+				return solveMaze(row - 1, col);
+			}
+			// Right?
+			if(right) {
+				marked[row][col] = true;
+				return solveMaze(row, col + 1);
+			}
+		}
+		// We didn't reach the end of the maze, and we can't go anywhere.
+		// Time to backtrack.
+		return solveMaze(R.pop(), C.pop());
+	}
+
 	public static void init() throws IOException {
 		Scanner scan = new Scanner(new File("Prob12.in.txt"));
+		List<Character[]> mazeTemp = new ArrayList<Character[]>();
 		while(scan.hasNext()) {
 			String next = scan.nextLine();
-			Boolean[] row = new Boolean[next.length()];
+			Character[] row = new Character[next.length()];
 			for(int i = 0, x = row.length; i < x; i++) {
-				row[i] = next.charAt(i) == 'O';
+				row[i] = next.charAt(i);
 			}
-			maze.add(row);
+			mazeTemp.add(row);
 		}
-		// Write a recursive call that stores the path into a path array
-		M = maze.size();
-		N = maze.get(0).length;
+		M = mazeTemp.size();
+		N = mazeTemp.get(0).length;
+		maze = new Character[M][N];
+		marked = new boolean[M][N];
+		for(int i = 0; i < M; i++) {
+			maze[i] = mazeTemp.get(i);
+		}
 		scan.close();
 	}
 }
